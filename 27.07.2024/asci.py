f8b933941134f54826e16f0d8aa79b690ac1fbd1
Allwin jerome <144081105+Castret@users.noreply.github.com>
2024-07-27 16:59:56 +0530

    Create asci.py

---
+++ 27.07.2024/asci.py
@@ -0,0 +1,7 @@
+# A to Z
+for char in range(ord('A'), ord('Z') + 1):
+    print(f"The ASCII value of {chr(char)} is {char}")
+#sample 2 
+character = input("Enter a character: ")
+ascii_value = ord(character)
+print("The ASCII value of", character, "is", ascii_value)
